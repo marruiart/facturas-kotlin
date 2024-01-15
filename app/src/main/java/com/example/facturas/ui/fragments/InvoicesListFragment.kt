8882c491d5acfecbfb8e23517117b41717c64052
Marina Ruiz Artacho <marruiart@gmail.com>
2024-01-15 08:40:22 +0100

    Feature top navigation bar

--- app/src/main/java/com/example/facturas/ui/fragments/InvoicesListFragment.kt
+++ app/src/main/java/com/example/facturas/ui/fragments/InvoicesListFragment.kt
@@ -6,10 +6,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.viewModels
+import com.example.facturas.R
 import com.example.facturas.databinding.FragmentInvoicesListBinding
 import com.example.facturas.ui.adapters.InvoicesListAdapter
 import com.example.facturas.ui.viewmodels.InvoicesListViewModel
+import com.google.android.material.color.MaterialColors
 
 class InvoicesListFragment : Fragment() {
     private lateinit var binding: FragmentInvoicesListBinding
@@ -26,7 +29,13 @@ class InvoicesListFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        val toolbar = binding.toolbar
+        toolbar.inflateMenu(R.menu.invoices_list_toolbar_menu)
+        // Change the navigation icon color
+        toolbar.navigationIcon?.setTint(MaterialColors.getColor(view, com.google.android.material.R.attr.colorPrimary))
+        toolbar.setNavigationOnClickListener {
+            Toast.makeText(context, context?.getString(R.string.not_available_yet), Toast.LENGTH_SHORT).show()
+        }
         val adapter = InvoicesListAdapter()
         bindView(adapter)
     }
